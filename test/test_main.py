1cc05623a7d5ee4db1f7762ae7ad246ecf442703
Jason Hutson <110316760+HutsonJason@users.noreply.github.com>
2023-03-30 20:02:09 -0400

    Finish writing get_file_message tests.

--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,82 @@
-import pytest
-import streamlit as st
-
 from campground_moving_to.main import get_file_status_message
 
 
-def test_get_file_status_message():
-    file_names_list = ["Due Out Report.csv"]
-    assert get_file_status_message(file_names_list) == (
-        "error",
-        "'Due Out Report.csv' needs to be uploaded too.",
-    )
+class TestGetFileStatusMessage:
+    def test_zero_file_names(self):
+        """Test for 0 file names in list."""
+        file_names_list = []
+        assert get_file_status_message(file_names_list) == (
+            "info",
+            "Please upload the Due In and Due Out Reports.",
+        )
+
+    def test_one_file_name(self):
+        """Test for 1 file name in list."""
+        file_names_list = ["Due In Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "'Due Out Report.csv' needs to be uploaded too.",
+        )
+        file_names_list = ["Due Out Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "'Due In Report.csv' needs to be uploaded too.",
+        )
+        file_names_list = ["Wrong file.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Please only upload 'Due In Report.csv' or 'Due Out Report.csv'.",
+        )
+
+    def test_two_file_names(self):
+        """Test for 2 file names in list."""
+        # Correct files.
+        file_names_list = ["Due In Report.csv", "Due Out Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "success",
+            "Found the correct files!",
+        )
+        file_names_list = ["Due Out Report.csv", "Due In Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "success",
+            "Found the correct files!",
+        )
+        # Wrong file uploaded.
+        file_names_list = ["Wrong file.csv", "Due In Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Please only upload 'Due In Report.csv' or 'Due Out Report.csv'.",
+        )
+        file_names_list = ["Due In Report.csv", "Wrong file.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Please only upload 'Due In Report.csv' or 'Due Out Report.csv'.",
+        )
+        file_names_list = ["Wrong file.csv", "Another wrong file.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Please only upload 'Due In Report.csv' or 'Due Out Report.csv'.",
+        )
+        # Duplicate file uploaded.
+        file_names_list = ["Due In Report.csv", "Due In Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Duplicate reports uploaded. Please upload 1 'Due In Report.csv' and 1 'Due Out Report.csv'",
+        )
+        file_names_list = ["Due Out Report.csv", "Due Out Report.csv"]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Duplicate reports uploaded. Please upload 1 'Due In Report.csv' and 1 'Due Out Report.csv'",
+        )
+
+    def test_more_than_two_file_names(self):
+        """Check for more than 2 file names in list."""
+        file_names_list = [
+            "Due In Report.csv",
+            "Due Out Report.csv",
+            "Another report.csv",
+        ]
+        assert get_file_status_message(file_names_list) == (
+            "error",
+            "Too many files uploaded. Remove extras.",
+        )
